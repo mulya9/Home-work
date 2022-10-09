a67fa81a0e899e30f67d0dd3312dd1c2d882a1be
Music <kolodiy83@gmail.com>
2022-10-09 19:16:04 +0300

    homework 3

---
+++ Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    public void start(){
+        start();
+    }
+    private void startElectricity(){
+
+    }
+    private void  startCommand(){
+
+    }
+
+    private void startFuelSystem(){
+    }
+}
---
+++ Employees.java
@@ -0,0 +1,15 @@
+public class Employees {
+   final String full_name;
+   final String job_title;
+   final int email;
+   final int telephone;
+   final int age;
+
+    public Employees(String full_name, String job_title, int email, int telephone, int age) {
+        this.full_name = full_name;
+        this.job_title = job_title;
+        this.email = email;
+        this.telephone = telephone;
+        this.age = age;
+    }
+}
